f93e50c76431994d0a6b2d53bb147e479c9d2760
Egor Babko <36426383@mail.ru>
2024-12-16 21:50:34 +0300

    Adaptive updated, reviews section added

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ menuClose = () => {
     document.querySelector(".header__burger").style.display = "block"
     document.querySelector(".header__cross").style.display = "none"
 }
+
+
+
+Promise.all([
+fetch("https://rickandmortyapi.com/api/character"),
+fetch('https://jsonplaceholder.typicode.com/todos')
+])
+    .then(responses => Promise.all(responses.map(res => res.json())))
+    .then(data => Reviews(data[0].results, data[1]))
+
+
+function Reviews(clients, description) {
+    const reviewsRow = document.querySelector('.reviews__row')  
+    console.log(description)
+    for (i = 0; i < 3; i ++) {
+        reviewsRow.innerHTML = reviewsRow.innerHTML + 
+        `
+        <div class="reviews__item">
+            <div class="reviews__info">
+                <img class="reviews__image" src="${clients[i].image}" alt="satisfied client">
+                <p class="reviews__name">${clients[i].name}</p>
+            </div>
+            <p class="reviews__review">${description[i].title}</p>
+        </div>
+        `
+    }
+}
